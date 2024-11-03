3f97c055d153389d1bde5a3cfea88738e4263ef3
Renato Ruppel <heruppel2@gmail.com>
2024-11-03 15:35:12 -0300

    feat(repositories, users): add fetch repository method

--- src/repositories/users.go
+++ src/repositories/users.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"api/src/models"
 	"database/sql"
+	"fmt"
 )
 
 type UsersRepository struct {
@@ -36,4 +37,50 @@ func (usersRepository UsersRepository) Create(user models.User) (uint64, error) 
 	}
 
 	return uint64(ID), nil
+}
+
+func (usersRepository UsersRepository) Fetch(nameOrNick string) ([]models.User, error) {
+	nameOrNick = fmt.Sprintf("%%%s%%", nameOrNick)
+
+	query := `SELECT 
+		id, 
+		name, 
+		nick, 
+		email, 
+		"createdAt" 
+	FROM users 
+	WHERE name ILIKE $1 OR nick ILIKE $2`
+
+	rows, err := usersRepository.db.Query(
+		query,
+		nameOrNick,
+		nameOrNick,
+	)
+
+	fmt.Println("CHEUGFIE AQUI O")
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []models.User
+
+	for rows.Next() {
+		var user models.User
+
+		if err = rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Nick,
+			&user.Email,
+			&user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }
