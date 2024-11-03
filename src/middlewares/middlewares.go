a1c79e7183c7eb68c7d842f683a8fd25fba4a134
Renato Ruppel <heruppel2@gmail.com>
2024-11-03 19:50:45 -0300

    feat(middlewares): authentication middleware

---
+++ src/middlewares/middlewares.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"api/src/authentication"
+	"api/src/responses"
+	"log"
+	"net/http"
+)
+
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.Host, r.RequestURI)
+		next(w, r)
+	}
+}
+
+func Authenticate(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := authentication.ValidateToken(r); err != nil {
+			responses.Error(w, http.StatusUnauthorized, err)
+			return
+		}
+		next(w, r)
+	}
+}
