0545e9a4d4b499dc36c33ca6d49d1ac56142646f
Renato Ruppel <heruppel2@gmail.com>
2024-11-03 15:55:36 -0300

    refact(models, user): condition to validate password

--- src/models/User.go
+++ src/models/User.go
@@ -15,8 +15,8 @@ type User struct {
 	CreatedAt time.Time `json:"createdAt,omitempty"`
 }
 
-func (user *User) Prepare() error {
-	if err := user.validate(); err != nil {
+func (user *User) Prepare(step string) error {
+	if err := user.validate(step); err != nil {
 		return err
 	}
 
@@ -24,7 +24,7 @@ func (user *User) Prepare() error {
 	return nil
 }
 
-func (user *User) validate() error {
+func (user *User) validate(step string) error {
 	if user.Name == "" {
 		return errors.New("NAME_REQUIRED")
 	}
@@ -34,7 +34,7 @@ func (user *User) validate() error {
 	if user.Email == "" {
 		return errors.New("EMAIL_REQUIRED")
 	}
-	if user.Password == "" {
+	if step == "CREATE" && user.Password == "" {
 		return errors.New("PASSWORD_REQUIRED")
 	}
 
