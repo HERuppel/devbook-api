05de08e2fe9acd2cd9b06749e5687087c8bdce7e
Renato Ruppel <heruppel2@gmail.com>
2024-11-03 20:07:32 -0300

    feat(sql): new table followers

--- sql/sql.sql
+++ sql/sql.sql
@@ -4,10 +4,20 @@ USE devbook;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   nick VARCHAR(50) NOT NULL unique,
   email VARCHAR(50) NOT NULL unique,
   password VARCHAR(255) NOT NULL,
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP()  
+);
+
+CREATE TABLE followers(
+  userId INTEGER not null,
+  followerId INTEGER not null,
+
+  PRIMARY KEY(userId, followerId),
+  
+  constraint fk_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE,
+  constraint fk_followerId FOREIGN KEY (followerId) REFERENCES users(id) ON DELETE CASCADE
 );
