9e1ddaeba538d120edc251dfccb23681258441a8
Renato Ruppel <heruppel2@gmail.com>
2024-11-03 20:59:53 -0300

    feat(controllers, auth): add update password

--- src/controllers/auth.go
+++ src/controllers/auth.go
@@ -8,8 +8,12 @@ import (
 	"api/src/responses"
 	"api/src/security"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -51,4 +55,64 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte(token))
+}
+
+func UpdatePassword(w http.ResponseWriter, r *http.Request) {
+	tokenUserId, err := authentication.ExtractUserId(r)
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	params := mux.Vars(r)
+	id, err := strconv.ParseUint(params["id"], 10, 64)
+	if err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if tokenUserId != id {
+		responses.Error(w, http.StatusForbidden, errors.New("CANNOT_UPDATE_PASSWORD_OF_OTHER"))
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+
+	var password models.Password
+	if err = json.Unmarshal(body, &password); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	usersRepository := repositories.NewUserRepository(db)
+	dbPassword, err := usersRepository.GetPassword(id)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err = security.VerifyPassword(dbPassword, password.OldPassword); err != nil {
+		responses.Error(w, http.StatusUnauthorized, errors.New("WRONG_PASSWORD"))
+		return
+	}
+
+	hash, err := security.Hash(password.NewPassword)
+	if err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err = usersRepository.UpdatePassword(id, string(hash)); err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusNoContent, nil)
 }
