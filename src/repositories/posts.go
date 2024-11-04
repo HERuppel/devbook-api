deed7b65b91a0d6ea19c9f9b29d1484e0462f1f2
Renato Ruppel <heruppel2@gmail.com>
2024-11-03 22:23:27 -0300

    feat(repositories, posts): add fetch

--- src/repositories/posts.go
+++ src/repositories/posts.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"api/src/models"
 	"database/sql"
+	"fmt"
 )
 
 type PostsRepository struct {
@@ -60,4 +61,46 @@ func (postsRepository PostsRepository) Get(id uint64) (models.Post, error) {
 	}
 
 	return post, nil
+}
+
+func (postsRepository PostsRepository) Fetch(userId uint64) ([]models.Post, error) {
+	query := `
+		SELECT DISTINCT 
+			p.*,
+			u.nick
+		FROM posts p
+		INNER JOIN users u ON p.authorid = u.id
+		LEFT JOIN followers f ON p.authorid = f.userId
+		WHERE u.id = $1 OR f.followerId = $2
+		ORDER BY p.id DESC
+	`
+
+	rows, err := postsRepository.db.Query(query, userId, userId)
+	if err != nil {
+		return []models.Post{}, err
+	}
+	defer rows.Close()
+
+	var posts []models.Post
+
+	for rows.Next() {
+		var post models.Post
+
+		if err = rows.Scan(
+			&post.ID,
+			&post.Title,
+			&post.Content,
+			&post.AuthorID,
+			&post.Likes,
+			&post.CreatedAt,
+			&post.AuthorNick,
+		); err != nil {
+			return []models.Post{}, err
+		}
+
+		posts = append(posts, post)
+	}
+
+	fmt.Println("🚀 ~ file: posts.go ~ line 101 ~ forrows.Next ~ posts : ", posts)
+	return posts, nil
 }
