deba9e7cf483ec3ec20e932c137d48ea3f651c88
Renato Ruppel <heruppel2@gmail.com>
2024-11-03 21:42:48 -0300

    feat(controller, posts): add create

---
+++ src/controllers/posts.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+	"api/src/authentication"
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"api/src/responses"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+	id, err := authentication.ExtractUserId(r)
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var post models.Post
+	if err = json.Unmarshal(body, &post); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	post.AuthorID = id
+
+	if err = post.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	postsRepository := repositories.NewPostsRepository(db)
+	post.ID, err = postsRepository.Create(post)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, post)
+}
+
+func FetchPosts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeletePost(w http.ResponseWriter, r *http.Request) {
+
+}
