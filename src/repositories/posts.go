ae7b83aa73b551b7e1751f4f7fbf9f6c843c6545
Renato Ruppel <heruppel2@gmail.com>
2024-11-03 21:58:12 -0300

    feat(repositories, posts): add get method

--- src/repositories/posts.go
+++ src/repositories/posts.go
@@ -32,3 +32,32 @@ func (postsRepository PostsRepository) Create(post models.Post) (uint64, error) 
 
 	return uint64(ID), nil
 }
+
+func (postsRepository PostsRepository) Get(id uint64) (models.Post, error) {
+	query := `
+		SELECT p.*, u.nick FROM posts p INNER JOIN users u on p.authorid = u.id WHERE id = $1
+	`
+
+	row, err := postsRepository.db.Query(query, id)
+	if err != nil {
+		return models.Post{}, err
+	}
+	defer row.Close()
+
+	var post models.Post
+	if row.Next() {
+		if err = row.Scan(
+			&post.ID,
+			&post.Title,
+			&post.Content,
+			&post.AuthorID,
+			&post.Likes,
+			&post.CreatedAt,
+			&post.AuthorNick,
+		); err != nil {
+			return models.Post{}, err
+		}
+	}
+
+	return post, nil
+}
